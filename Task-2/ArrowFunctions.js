46c95d520af750f6cb7a4f2513f6893b01047d72
rushi patel <rushinasa06@gmail.com>
2021-11-02 10:08:15 +0530

    requested changes done

---
+++ Task-2/ArrowFunctions.js
@@ -0,0 +1,5 @@
+
+concancate_string = (string1,string2) => {return string1+string2;}
+
+const merged_string = concancate_string("HELLO","WORLD")
+console.log(merged_string)
--- Task-2/Arrow_Functions.js
+++
@@ -1,7 +0,0 @@
-
-concancate_string = (string1,string2) => {
-    return string1+string2;
-}
-
-let merged_string = concancate_string("HELLO","WORLD")
-console.log(merged_string)
